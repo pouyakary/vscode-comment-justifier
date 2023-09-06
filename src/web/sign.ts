8afc3b8b9a932361280c5321bf2e94848075303a
Pouya Kary ✨ <kary@gnu.org>
2023-09-06 15:57:20 +0330

    added license

--- src/web/sign.ts
+++ src/web/sign.ts
@@ -1,3 +1,32 @@
+// Justifier - A comment justifier
+// extension for Visual Studio Code that
+// justifies the size of comments lines to
+// under 40 character for better
+// readability.
+//
+// (C) 2023-present Pouya Kary
+// <kary@gnu.org>
+//
+// This program is free software: you can
+// redistribute it and/or modify it under
+// the terms of the GNU General Public
+// License as published by the Free
+// Software Foundation, either version 3 of
+// the License, or (at your option) any
+// later version.
+//
+// This program is distributed in the hope
+// that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the
+// GNU General Public License along with
+// this program. If not, see
+// <https://www.gnu.org/licenses/>.
+
 const oneLineCommentSigns = [
   '//', '///', '--', '#', ';;', '*', '/**', '/*'
 ]
