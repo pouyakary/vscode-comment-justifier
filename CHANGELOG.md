0e271f02ddd8500a2ed12e8ef82eb67a26e840b2
Pouya Kary ✨ <kary@gnu.org>
2024-01-14 16:29:41 +0330

    A new algorithm that breaks the words

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Change Log
 
+- `0.0.9` &mdash; An awesome new algorithm patch that breaks the chunks if the empty space is too much and makes the results really pretty.
+
 - `0.0.8` &mdash; The algorithm now justifies the words too. (the previous versions used to justify by left and keep spaces as they were, the new version inserts more spaces to make the lines equal)
 
 - `0.0.6` &mdash; Improved the algorithm to handle orphan words at the last line.
--- src/web/justification.ts
+++ src/web/justification.ts
@@ -1,31 +1,31 @@
-// Justifier - A comment justifier
-// extension for Visual Studio Code that
-// justifies the size of comments lines to
-// under 40 character for better
-// readability.
+// Justifier    -   A   comment   justifier
+// extension for Visual  Studio  Code  that
+// justifies  the size of comments lines to
+// under 40 character for
+// better readability.
 //
 // (C) 2023-present Pouya Kary
 // <kary@gnu.org>
 //
-// This program is free software: you can
-// redistribute it and/or modify it under
-// the terms of the GNU General Public
-// License as published by the Free
+// This  program  is free software: you can
+// redistribute it and/or modify  it  under
+// the  terms  of  the  GNU  General Public
+// License  as  published   by   the   Free
 // Software Foundation, either version 3 of
-// the License, or (at your option) any
+// the License, or  (at  your  option)  any
 // later version.
 //
-// This program is distributed in the hope
-// that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied
-// warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU
+// This  program is distributed in the hope
+// that it will be useful, but WITHOUT  ANY
+// WARRANTY;   without   even  the  implied
+// warranty of MERCHANTABILITY  or  FITNESS
+// FOR  A  PARTICULAR  PURPOSE. See the GNU
 // General Public License for more details.
 //
-// You should have received a copy of the
-// GNU General Public License along with
-// this program. If not, see
-// <https://www.gnu.org/licenses/>.
+// You  should  have received a copy of the
+// GNU General Public  License  along  with
+// this program. If not,
+// see <https://www.gnu.org/licenses/>.
 
 import { detectStartOfTheComment } from "./sign"
 
@@ -35,21 +35,39 @@ const maxLineSize = 40;
 
 // ─── Gets The Words Of The Comment ─────────────────────────────────────── ✣ ─
 
-function*
-extractChunks(lines: string[]): Generator<string> {
+function
+extractChunks(lines: string[]): string[] {
+  const chunks = new Array<string>();
+
   for (const index in lines) {
     var line = lines[index]
     var line = line.replace(/^\s*\/+/, "")
 
     for (const chunk of line.split(/\s+/)) {
       if (chunk != '') {
-        yield chunk
+        chunks.push(chunk);
       }
     }
   }
+  return chunks;
 }
 
 // ─── Justifies A Given Comment ─────────────────────────────────────────── ✣ ─
+
+function
+splitChunkInHalf(chunk: string, availableSpace: number): [string, string] {
+    // one  char for the space to be before the
+    // chunk and one for the hyphen after it.
+    const availableSpaceForChunk = availableSpace - 2
+
+    // trying to at least preserve 3 characters
+    // of the word
+    const headSize = Math.min(availableSpaceForChunk, chunk.length - 3)
+
+    var firstHalf = chunk.substring(0, headSize);
+    var secondHalf = chunk.substring(headSize);
+    return [firstHalf, secondHalf];
+}
 
 export function
 justify(input: string[]): string {
@@ -57,21 +75,39 @@ justify(input: string[]): string {
   const commentStart 	  = detectStartOfTheComment(input[0])
   let   buffer          = new Array<string>();
   const bufferLength    = (): number => buffer.join(' ').length;
+  const chunksReverse   = extractChunks(input).reverse();
 
-  // Justifiying with basic logic
-  for (const chunk of extractChunks(input)) {
-    if (bufferLength() + 1 + chunk.length > maxLineSize) {
+  // Justifying with basic logic
+  while (chunksReverse.length > 0) {
+    const chunk = chunksReverse.pop()!;
+    const lineSizeWithChunkAdded = bufferLength() + 1 + chunk.length;
+
+    let splittedAChunk = false
+
+    if (lineSizeWithChunkAdded > maxLineSize) {
+      const emptySize = maxLineSize - lineSizeWithChunkAdded + chunk.length
+
+      if (chunk.length >= 6 && emptySize >= 4) {
+        splittedAChunk = true
+        const [head, tail] = splitChunkInHalf(chunk, emptySize)
+        chunksReverse.push(tail);
+        buffer.push(`${head}-`)
+      }
+
       lines.push(buffer)
       buffer = new Array<string>();
     }
-    buffer.push(chunk);
+
+    if (splittedAChunk === false) {
+     buffer.push(chunk);
+    }
   }
 
   if (buffer.length > 0) {
     lines.push(buffer);
   }
 
-  // handling the orphan word case.
+  // handling the orphan case
   let lastLine = lines[lines.length - 1];
 
   if (lastLine.length == 1 && lines.length > 1) {
@@ -87,9 +123,11 @@ justify(input: string[]): string {
   return (commentStart + stringifiedLines.join("\n" + commentStart)).trimEnd();
 }
 
+
 // ─── Insert Spaces ─────────────────────────────────────────────────────── ✣ ─
 
-function insertSpaces(lines: string[][]): string[] {
+function
+insertSpaces(lines: string[][]): string[] {
   const resultLines = new Array<string>();
 
   for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
@@ -124,7 +162,7 @@ function insertSpaces(lines: string[][]): string[] {
 
     let result = '';
 
-    // We use this method here to as much as
+    // We  use  this  method here to as much as
     // possible avoid rivers.
     for (let j = 0; j < words.length - 1; j++) {
       let spaceIndex = lineIndex % 2 === 0 ? j : spacesNeeded - j - 1;
